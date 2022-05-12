86980f5f8568fdd32febc312fa2f6ba3b8d10e1b
RaviPatel-2795 <patel.ravi.2795@gmail.com>
2022-05-12 11:52:48 +0100

    React Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// LEGACY WAY TO INCLUDE JSX
+//import React from 'react';
 
-function App() {
+import "./App.css";
+import Expenses from "./components/RenderExpenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // LEGACY WAY TO INCLUDE JSX
+  // you can only return one thing in a function.
+  // Hence we NEED to only return ONE root element.
+  //
+  // return React.createElement(
+  //   'div',
+  //   {className: 'app-container'},
+  //   React.createElement('h2', {className: 'app-heading'}, "Ravi's Expense Tracker"),
+  //   React.createElement(Expenses, {items: expenses})
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h2 className="app-heading">Ravi's Expense Tracker</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
